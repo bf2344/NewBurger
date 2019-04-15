83e9a36c3135a01d4c02551393b2ad300844e1c8
Brian Ford <bford2344@gmail.com>
2019-04-15 09:09:21 -0600

    AHHSHGDHDJSGHDJSH

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var bodyParser = require("body-parser");
+// var bodyParser = require("body-parser");
 
 var PORT = process.env.PORT || 8080;
 var app = express();
@@ -8,9 +8,9 @@ var app = express();
 app.use(express.static("public"));
 
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 var exphbs = require("express-handlebars");
 
