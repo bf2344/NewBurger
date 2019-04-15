c2ed9fed1b68eea61faa850e7ea4cf5e9e74c1b5
Brian Ford <bford2344@gmail.com>
2019-04-15 09:00:39 -0600

    grr

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/BurgersController.js");
+var routes = require("./controllers/Burgerscontroller.js");
 
 app.use(routes);
 
